828d0c8492b5db31e7e67e10245d71e3447a34dc
Benjamin Martinez <benjm@fb.com>
2022-07-07 15:57:21 -0700

    Added basic webpage and dependencies to display Hello World

---
+++ frontend/src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import {
+  Routes, Route, Link, useParams, BrowserRouter,
+} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+export default function App () {
+
+
+    return (
+
+        <div className="app">Hello world and others</div>
+
+    );
+}
