b31de427988695c89335edfa793d4c25171f8187
Benjamin Martinez <benjm@fb.com>
2022-07-12 15:40:08 -0700

    Added basic FB Login API

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -3,13 +3,57 @@ import {
   Routes, Route, Link, useParams, BrowserRouter,
 } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import FacebookLogin from 'react-facebook-login';
+import { Card, Image } from 'react-bootstrap';
+
 
 export default function App () {
 
 
-    return (
+  const [login, setLogin] = useState(false);
+  const [data, setData] = useState({});
+  const [picture, setPicture] = useState('');
 
-        <div className="app">Hello world and others</div>
+  const responseFacebook = (response) => {
+    console.log(response);
+    setData(response);
+    setPicture(response.picture.data.url);
+    if (response.accessToken) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  }
+  return (
 
-    );
+    <div className="app">
+      <Card style={{ width: '600px' }}>
+        <Card.Header>
+          {!login &&
+            <FacebookLogin
+              appId="766038284415266"
+              autoLoad={true}
+              fields="name,email,picture"
+              scope="public_profile,user_friends"
+              callback={responseFacebook}
+              icon="fa-facebook" />
+          }
+          {login &&
+            <Image src={picture} roundedCircle />
+          }
+        </Card.Header>
+        {login &&
+          <Card.Body>
+            <Card.Title>{data.name}</Card.Title>
+            <Card.Text>
+              {data.email}
+            </Card.Text>
+          </Card.Body>
+        }
+      </Card>
+    </div>
+
+  );
 }
