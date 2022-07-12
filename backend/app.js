7646d4232c37e53dd3b829a54a7e7c50544593e0
Benjamin Martinez <benjm@fb.com>
2022-07-12 15:58:48 -0700

    Added basic login with Parse

--- backend/app.js
+++ backend/app.js
@@ -11,4 +11,35 @@ Parse.initialize(
 
 Parse.serverURL = "https://parseapi.back4app.com"
 
+app.use(express.json())
+app.use(morgan("tiny"))
+app.use(cors())
 
+
+// Expects parameters for username and password
+app.post('/login', async (req, res) => {
+    try {
+      const user = await Parse.User.logIn(req.body.username, req.body.password)
+      res.send({"user" : user})
+    } catch (error) {
+      res.status(400)
+      res.send({"error" : "Login failed: " + error })
+    }
+  })
+  
+  // Expects paramters for username, password, and any additional
+  // columns that you want in your user table (profile_picture_url, etc)
+  app.post('/register', async (req, res) => {
+    let user = new Parse.User(req.body)
+  
+    try {
+        await user.signUp()
+        res.status(201)    
+        res.send({"user" : user})
+    } catch (error) {
+        res.status(400)
+        res.send({"error" : "Failed to create user: " + error })
+    }
+  })
+
+  module.exports = app;
