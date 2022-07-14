462f4e41959a3e5c4e504a41bb0739f315ec096a
Benjamin Martinez <benjm@fb.com>
2022-07-14 14:11:02 -0700

    Set up React Quill Text Editor

--- frontend/src/components/Wiki/Wiki.jsx
+++ frontend/src/components/Wiki/Wiki.jsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import './Wiki.css';
+import { useState } from "react";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 export default function Wiki() {
+
+    const [value, setValue] = useState('');
 
     return (
       <div className='wiki'>
         Wiki
+        <ReactQuill theme="snow" value={value} onChange={setValue}/>
+
       
       </div>
     );
