10adc1750281b6c33cb00658b49718eb326e781d
Benjamin Martinez <benjm@fb.com>
2022-07-28 11:36:31 -0700

    Added card to search results

--- frontend/src/components/SearchResults/SearchResults.jsx
+++ frontend/src/components/SearchResults/SearchResults.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './SearchResults.css'
 import { Link } from 'react-router-dom';
+import { Card } from 'react-bootstrap';
 
 export default function SearchResults({ searchResults }) {
     console.log('searchResults: ', searchResults);
@@ -17,17 +18,21 @@ export default function SearchResults({ searchResults }) {
                     if (result.userID) {
                         return (
                             <div className='link-user' key={result.userID}>
+                            <Card><Card.Body>
                                 <Link to={"/account/" + result.userID}>
                                     User: {result.user_name}
                                 </Link>
+                            </Card.Body></Card>
                             </div>
                         )
                     } else if (result.objectId) {
                         return (
                             <div className='link-wiki' key={result.objectId}>
+                            <Card><Card.Body>
                                 <Link to={"/wiki/" + result.objectId}>
                                     Wiki: {result.wikiObject.title}
                                 </Link>
+                            </Card.Body></Card>
                             </div>
                         )
                     } else {
